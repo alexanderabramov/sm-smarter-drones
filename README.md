4fc689fee4d86d14b2dccdc7bd7788ecdda41256
Alexander Abramov <alexander.abramov.pub@gmail.com>
2018-09-24 20:07:49 +0200

    steamid, preview image, readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# sm-smarter-drones
-Smarter Drones mod for Surviving Mars
+# Smarter Drones
+Smarter Drones is a mod for a game Surviving Mars by Haemimont Games.
+
+It intends to improves Drone AI in small but crucial ways.
+
+First improvement:
+- when dumping Waste Rock, drones carry it to the closest available Dumping Site or pile (unlike round-robin in the base game). This is an enormous efficiency boost if you have more than one source of waste rock in range of one drone controller.
+
+Compatibility:
+- does not modify any game objects, so should be compatible with almost any mods, and can be added/removed to existing saves
+- modifies some functions in Drone and DroneControl classes, could break after game updates
--- metadata.lua
+++ metadata.lua
@@ -1,14 +1,16 @@
 return PlaceObj('ModDef', {
 	'title', "Smarter Drones",
 	'description', "Improves Drone AI in small but crucial ways.\r\n\r\nFirst improvement:\r\n- when dumping Waste Rock, drones carry it to the closest available Dumping Site or pile (unlike round-robin in the base game). This is an enormous efficiency boost if you have more than one source of waste rock in range of one drone controller.\r\n\r\nCompatibility:\r\n- does not modify any game objects, so should be compatible with almost any mods, and can be added/removed to existing saves\r\n- modifies some functions in Drone and DroneControl classes, could break after game updates",
+	'image', "preview.png",
 	'id', "Yc1438O",
+	'steam_id', "1358005563",
 	'author', "casual",
-	'version', 3,
+	'version', 12,
 	'lua_revision', 228722,
 	'code', {
 		"Code/Drone.lua",
 		"Code/DroneHub.lua",
 	},
-	'saved', 1523387280,
+	'saved', 1523389528,
 	'TagOther', true,
 })
